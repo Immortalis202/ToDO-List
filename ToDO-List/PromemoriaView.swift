1f9ca8300412087a74424ffb8db391c659d97fab
Luca <lmalegori@gmail.com>
2024-10-15 17:14:18 +0200

    Animation

--- ToDO-List/PromemoriaView.swift
+++ ToDO-List/PromemoriaView.swift
@@ -1,6 +1,8 @@
+
+
 import SwiftUI
 
-struct Riga: Identifiable {
+struct Riga: Identifiable,Equatable {
     let id: String
     var testo: String
     var fatto: Bool
@@ -11,16 +13,15 @@ struct PromemoriaView: View {
     @State private var mostraDaFare: Bool = false
     @State private var testo: String = ""
     
-    var filtrati:[Riga] {
-        elenco.filter { !$0.fatto || !mostraDaFare }
-    }
-    
-    
     @State private var elenco: [Riga] = [
         Riga(id: "1", testo: "Da fare 1", fatto: false),
         Riga(id: "2", testo: "Da fare 2", fatto: true),
         Riga(id: "3", testo: "Da fare 3", fatto: false),
     ]
+    
+    private var filtrati: [Riga] {
+        elenco.filter { !$0.fatto || !mostraDaFare }
+    }
     
     var body: some View {
         List {
@@ -42,12 +43,12 @@ struct PromemoriaView: View {
                         if let index = elenco.firstIndex(where: { $0.id == elemento.id }) {
                             elenco[index].fatto = newValue
                         }
-                    })) {
+                    }))
+                {
                     Text(elemento.testo)
                 }
             }
-            
-        }
+        }.animation(.default, value: filtrati)
     }
 }
 
